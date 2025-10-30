a8d92e48c8abe7a171348e6666912beb79f4553e
Vincent Halder <vincent@MacBook-Air-von-Vincent.local>
2025-10-30 14:19:58 +0100

    More changes

---
+++ Project/Scripts/01_Data_Cleaning.R
@@ -0,0 +1,8 @@
+source("Project/Scripts/05_Packages.R")
+titanic <- as_tibble(read.csv("Project/Data/Raw/titanic.csv"))
+
+view(titanic)
+summary(titanic)
+str(titanic)
+
+
---
+++ Project/Scripts/05_Packages.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+library(purrr)
+library(here)
+library(ggplot2)
+library(broom)
