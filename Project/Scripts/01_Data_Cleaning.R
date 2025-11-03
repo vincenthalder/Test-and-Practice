08d1b0edb97353e108c7718c0f22d4a33f8de369
Vincent Halder <vincent@MacBook-Air-von-Vincent.local>
2025-11-03 18:59:42 +0100

    first steps in data wrangling

--- Project/Scripts/01_Data_Cleaning.R
+++ Project/Scripts/01_Data_Cleaning.R
@@ -1,8 +1,31 @@
 source("Project/Scripts/05_Packages.R")
-titanic <- as_tibble(read.csv("Project/Data/Raw/titanic.csv"))
+titanic_raw <- as_tibble(read.csv("Project/Data/Raw/titanic.csv"))
 
+## Important First Cleaning Step: Put NA's where empty cells are empty character strings.
+titanic_raw <- titanic_raw %>%
+  mutate(across(where(is.character), ~ na_if(.x, "")))
+
+## Now inspect 
 view(titanic)
 summary(titanic)
 str(titanic)
+glimpse(titanic)
+
+
+## (1) Building a Data-Audit Table 
+audit_table <- tibble(
+  vars = colnames(titanic_raw),
+  number_non_missing = titanic_raw |> 
+    summarise(across(everything(), ~ sum(!is.na(.x)))) |> 
+    unlist(use.names = FALSE)
+)
+
+
+?count
+## (2) Clean Names to snake case 
+titanic_clean <- titanic_raw |>  
+  clean_names(case = "snake") |> 
+  mutate(across(where(is.character), trimws)) 
+  
 
 
